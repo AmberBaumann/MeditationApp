c557bda9532392192f285fdd25bc09efa2f48615
Amber Baumann <ambermindsolliving@gmail.com>
2021-02-19 17:43:25 -0600

    Initial commit

---
+++ Meditation/src/main/java/org/launchcode/Meditation/MeditationApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Meditation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditationApplication.class, args);
+	}
+
+}
---
+++ Meditation/src/test/java/org/launchcode/Meditation/MeditationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Meditation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
